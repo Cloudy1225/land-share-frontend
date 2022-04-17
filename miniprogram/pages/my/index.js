8e5c018cee29832d9bfb4c5818fc85ceea3317e0
Cloudy1225 <2034885420@qq.com>
2022-04-18 00:33:41 +0800

    简单实现登录功能

--- miniprogram/pages/my/index.js
+++ miniprogram/pages/my/index.js
@@ -5,7 +5,57 @@ Page({
      * 页面的初始数据
      */
     data: {
+        userInfo: [],
+        defaultAvatar: "../../images/unloginAvatar.png",
+        hasUserInfo: false
+    },
 
+    // 点击登录函数
+    login() {
+      this.register(); 
+      wx.getUserProfile({
+        desc: '获取你的头像和昵称:)',
+        success: (res) => {
+          // console.log(res);
+          this.setData({ // 设置用户信息：头像、昵称、性别等
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    },
+    
+    // 看是否为新用户，如果是则自动注册
+    register(){
+      const res =  wx.cloud.callContainer({
+        config: {
+          env: 'prod-9grx0olg9c8cf232', // 微信云托管的环境ID
+        },
+        path: '/my/loginOrRegister', // 填入业务自定义路径和参数
+        method: 'GET', // 按照自己的业务开发，选择对应的方法
+        header: {
+          'X-WX-SERVICE': 'land-share-test', // xxx中填入服务名称（微信云托管 - 服务管理 - 服务列表 - 服务名称）
+          // 其他header参数
+        }
+        // 其余参数同 wx.request
+      });
+      // console.log(res);
+    },
+
+    inc(){
+      wx.cloud.callContainer({
+        "config": {
+          "env": "prod-9grx0olg9c8cf232"
+        },
+        "path": "/api/count",
+        "header": {
+          "X-WX-SERVICE": "land-share-test"
+        },
+        "method": "POST",
+        "data": {
+          "action": "inc"
+        }
+      })
     },
 
     /**
