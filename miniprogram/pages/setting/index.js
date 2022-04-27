3b85e7855f94db3e688c52c0e33e3ae295d7afc6
nianbai <20307130130@fdu.edu.cn>
2022-04-27 21:13:15 +0800

    code for the setting interface:logout and concel account

--- miniprogram/pages/setting/index.js
+++ miniprogram/pages/setting/index.js
@@ -1,18 +1,68 @@
 // pages/setting/index.js
+const app = getApp();
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
+ 
+    },
 
+
+    //退出登录函数
+    logout(){
+        wx.showModal({
+            title: '提示',
+            content: '确定退出登录吗，此操作会清空缓存',
+            cancelColor: 'cancelColor',
+            success: function(res){
+                if(res.confirm){
+                    app.globalData.role="0";
+                    app.globalData.hasUserInfo="false";
+                    wx.clearStorage({
+                      success: (res) => {},
+                    });
+                    wx.switchTab({
+                      url: '/pages/my/index',
+                    });
+                }
+            }
+        });
+
+
+    },
+
+    //注销账号函数
+    cancelAccount(){
+        wx.showModal({
+            title: '提示',
+            content: '您确定要注销账号吗，注销后无法恢复',
+            cancelColor: 'cancelColor',
+            success: function(res){
+                if(res.confirm){
+                    app.callContainer({
+                        path: '/my/deleteUser',
+                        method: 'GET'
+                    });
+                    app.globalData.role="0";
+                    app.globalData.hasUserInfo="false";
+                    wx.clearStorage({
+                      success: (res) => {},
+                    });
+                    wx.switchTab({
+                      url: '/pages/my/index',
+                    });
+                }
+            }
+        });
+        //console.log(res)
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
     },
 
     /**
